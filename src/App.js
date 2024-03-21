3e92af98953832726ce59a9b4cfb43bdc40ed985
varshithsamineni <114898379+varshithsamineni@users.noreply.github.com>
2024-03-21 22:30:13 +0530

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import {useState} from "react";
 
-function App() {
+const App = () => {
+  const [action,setAction]=useState("Sign Up");
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+      <div className="maindiv">
+        <div className="innerdiv">
+          <div className="header">
+            <h2>{action}</h2>
+            { (action==="Login")?<div></div>:
+            <div className="input1">
+            
+              <input type="text" name="name" placeholder="name"/>
+            </div>
 }
+            <div className="input1">
+              <input type="email" name="email" placeholder="email"/>
+            </div>
+            <div className="input1">
+              <input type="password" name="password" placeholder="password" />
+            </div>
+            <div className="button">
+              
+              <button className={action === "Sign Up" ? "submitgray" : "submit"} onClick={() => { setAction("Sign Up") }}>Sign up</button>
+              <button className={action === "Login" ? "submitgray" : "submit"} onClick={() => { setAction("Login") }}>Login</button>
+
+            </div>
+            {  (action==="Login")?
+            <div className="second">
+            <a href="#">forgot password?</a>
+          </div>:<div></div>  }
+          </div>
+          
+        </div>
+      </div>
+    </>
+  );
+};
 
 export default App;
